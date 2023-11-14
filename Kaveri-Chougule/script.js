5858cb766745a6f70dbc07aabc25a1899888e685
Kaveri Chougule <chougulekaveri123@gmail.com>
2023-11-14 19:39:32 +0530

    Add items issue

--- Kaveri-Chougule/script.js
+++ Kaveri-Chougule/script.js
@@ -4,7 +4,6 @@ let date = new Date();
 let day=date.getDate();
 let month=date.getMonth();
 let year=date.getFullYear()
-console.log(day);
 current_date.innerHTML=`
     ${day}/${month}/${year}
 `
@@ -22,44 +21,36 @@ function BtnAdd(){
 function BtnDelete(current){
     $(current).parent().parent().remove()
 }
-// let total_amount=0;
-// function calculation(ele){
-//     // let index=$(ele).parent().parent().index();
-//     let index=ele.target.value
-//     console.log(index);
-
-//     // let quantity=document.getElementsByName("quantity")[index].value
-//     // console.log(quantity);
-
-//     // let rate=document.getElementsByName("price")[index].value
-//     // console.log(rate);
-
-//     // total_amount+=quantity*rate
-//     // console.log(total_amount);
-//     // document.getElementById("total_amount").innerText=total_amount
-//     // console.log(total_amount);
-// }
 
 let tbody=document.getElementById("tbody")
 tbody.addEventListener("input",(event)=>{
+    let quantity=1;
+    let price=1.00;
     if(event.target.classList.contains("quantity")){
-        let quantity=event.target.value
-        console.log(quantity);
+        quantity=event.target.value
+        // console.log(quantity);
     }
     if(event.target.classList.contains("price")){
-        let price=event.target.value
-        console.log(price);
+        price=event.target.value
+        // console.log(price);
     }
+    quantity=parseFloat(quantity)
+    price=parseFloat(price)
+    // console.log(quantity);
+    console.log(price*quantity);
+    let taxRate=document.getElementById("taxRate")
+    let discountRate=document.getElementById("discountRate")
+
+    let amount=document.getElementById("amount")
+    amount.innerText=quantity*price
 })
 
 // changing currency sign
 let currency=document.querySelector(".currency")
 
 let currency_change=document.querySelectorAll(".currency_change")
-console.log(currency_change);
 currency.addEventListener("input",()=>{
     for(let i=0;i<currency_change.length;i++){
         currency_change[i].innerHTML=`${currency.value}`
     }
-    
 })

3dc78ed3a2ac051da19950b7911120f3f92c6368
Kaveri Chougule <chougulekaveri123@gmail.com>
2023-11-14 18:14:37 +0530

    Adding event issue

--- Kaveri-Chougule/script.js
+++ Kaveri-Chougule/script.js
@@ -22,3 +22,28 @@ function BtnAdd(){
 function BtnDelete(current){
     $(current).parent().parent().remove()
 }
+// let total_amount=0;
+// function calculation(ele){
+//     // let index=$(ele).parent().parent().index();
+//     let index=ele.target.value
+//     console.log(index);
+
+//     // let quantity=document.getElementsByName("quantity")[index].value
+//     // console.log(quantity);
+
+//     // let rate=document.getElementsByName("price")[index].value
+//     // console.log(rate);
+
+//     // total_amount+=quantity*rate
+//     // console.log(total_amount);
+//     // document.getElementById("total_amount").innerText=total_amount
+//     // console.log(total_amount);
+// }
+
+let table_row=document.getElementById("table-row")
+table_row.addEventListener("keypress",(event)=>{
+    if(event.target.className=== "quantity"){
+        let quantity=event.target.value
+        console.log();
+    }
+})

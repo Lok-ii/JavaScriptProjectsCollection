43d4534b5f4198dd07baa813a045fed80a59444a
Kaveri Chougule <chougulekaveri123@gmail.com>
2023-11-16 02:22:36 +0530

    Images size reduced

---
+++ script.js
@@ -0,0 +1,15 @@
+let obj={
+    project1:{
+        name:"Chrome Extension",
+        image:"https://images.unsplash.com/photo-1480694313141-fce5e697ee25?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    project2:{
+        name:"Resume Template",
+        image:"https://plus.unsplash.com/premium_photo-1661288470388-c5006797bdff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    project3:{
+        name:"Invoice Generator",
+        image:"https://plus.unsplash.com/premium_photo-1679784204535-e623926075cb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+}
+console.log(obj);

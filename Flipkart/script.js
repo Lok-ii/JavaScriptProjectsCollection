1928a8a1fc2edf59fb025d46351c31f110a82680
Lok-ii <lokeshkataria.work@gmail.com>
2023-11-16 10:02:12 +0530

    added drop down function

--- Flipkart/script.js
+++ Flipkart/script.js
@@ -1,23 +1,33 @@
-import {products} from "./data.js";
+import { products } from "./data.js";
 
-console.log(products);
+// let fList = [];
+// let price = [document.querySelector(".min"), document.querySelector(".max")];
 
-let addProducts = (list = products)=>{
-    const productList = document.querySelector(".product-list");
-    productList.innerHTML = "";
-    list.forEach((item)=>{
-        let freeDelivery = "";
-        let fAssured = "";
-        if(item.deliveryIn1Day === true){
-            freeDelivery = "Free Delivery"
-        }
-        if(item.flipkartAssured === true){
-            fAssured = "./Assets/fa_62673a.png"
-        }
-        let ratingValue = Math.floor(Math.random() * 5000);
-        let div = document.createElement("div");
-        div.classList.add("product-card");
-        div.innerHTML = `<i class="fa-solid fa-heart"></i>
+// let priceRange = document.querySelector(".range-price");
+
+// priceRange.addEventListener("input", (e)=>{
+//     if(e.target.className === "min" || e.target.className === "max"){
+//         console.log(price[0].value, price[1].value);
+//         console.log(price[0].innerText);
+//     }
+// })
+
+let addProducts = (list = products) => {
+  const productList = document.querySelector(".product-list");
+  productList.innerHTML = "";
+  list.forEach((item) => {
+    let freeDelivery = "";
+    let fAssured = "";
+    if (item.deliveryIn1Day === true) {
+      freeDelivery = "Free Delivery";
+    }
+    if (item.flipkartAssured === true) {
+      fAssured = "./Assets/fa_62673a.png";
+    }
+    let ratingValue = Math.floor(Math.random() * 5000);
+    let div = document.createElement("div");
+    div.classList.add("product-card");
+    div.innerHTML = `<i class="fa-solid fa-heart"></i>
         <img class="productImage" src="${item.image}" alt="">
         <span class="productName">${item.title}</span>
         <span class="productColor">${item.color}</span>
@@ -38,9 +48,9 @@ let addProducts = (list = products)=>{
         </div>
         <span class="delivery" name="Free">${freeDelivery}</span>`;
 
-        productList.appendChild(div);
-    });
-}
+    productList.appendChild(div);
+  });
+};
 
 let rangeMin = 100;
 const range = document.querySelector(".range-selected");
@@ -48,63 +58,67 @@ const rangeInput = document.querySelectorAll(".range-input input");
 const rangePrice = document.querySelectorAll(".range-price input");
 
 rangeInput.forEach((input) => {
-    input.addEventListener("input", (e) => {
-      let minRange = parseInt(rangeInput[0].value);
-      let maxRange = parseInt(rangeInput[1].value);
-      if (maxRange - minRange < rangeMin) {     
-        if (e.target.className === "min") {
-          rangeInput[0].value = maxRange - rangeMin;        
-        } else {
-          rangeInput[1].value = minRange + rangeMin;        
-        }
+  input.addEventListener("input", (e) => {
+    let minRange = parseInt(rangeInput[0].value);
+    let maxRange = parseInt(rangeInput[1].value);
+    if (maxRange - minRange < rangeMin) {
+      if (e.target.className === "min") {
+        rangeInput[0].value = maxRange - rangeMin;
       } else {
-        rangePrice[0].value = minRange;
-        rangePrice[1].value = maxRange;
-        range.style.left = (minRange / rangeInput[0].max) * 100 + "%";
-        range.style.right = 100 - (maxRange / rangeInput[1].max) * 100 + "%";
+        rangeInput[1].value = minRange + rangeMin;
       }
-    });
+    } else {
+      rangePrice[0].value = minRange;
+      rangePrice[1].value = maxRange;
+      range.style.left = (minRange / rangeInput[0].max) * 100 + "%";
+      range.style.right = 100 - (maxRange / rangeInput[1].max) * 100 + "%";
+    }
   });
+});
 
-  let sortItemsList = document.querySelectorAll(".sort-item");
- let sortItems = ()=>{
-    sortItemsList.forEach(element=>{
+let sortItemsList = document.querySelectorAll(".sort-item");
+let sortItems = () => {
+  sortItemsList.forEach((element) => {
     element.id = "";
   });
-}
+};
 
 //Sorting
 let sortOptions = document.querySelector(".sortOptions");
-sortOptions.addEventListener("click", (e)=>{
-let sortedList = "";
-    if(e.target.classList.contains("popularity")){
-        sortItems();
-        e.target.id = "selected";
-        console.log("clicked popularity");
-        sortedList = products.sort((a, b) => b.popularity - a.popularity);
-        addProducts(sortedList);
-    }else if(e.target.classList.contains("low-to-high")){
-        sortItems();
-        e.target.id = "selected";
-        sortedList = products.sort((a, b)=> a.specialPrice - b.specialPrice);
-        addProducts(sortedList);
-
-    }else if(e.target.classList.contains("high-to-low")){
-        sortItems();
-        e.target.id = "selected";
-        sortedList = products.sort((a, b)=> b.specialPrice - a.specialPrice);
-        addProducts(sortedList);
-
-    }
+sortOptions.addEventListener("click", (e) => {
+  let sortedList = "";
+  if (e.target.classList.contains("popularity")) {
+    sortItems();
+    e.target.id = "selected";
+    console.log("clicked popularity");
+    sortedList = products.sort((a, b) => b.popularity - a.popularity);
+    addProducts(sortedList);
+  } else if (e.target.classList.contains("low-to-high")) {
+    sortItems();
+    e.target.id = "selected";
+    sortedList = products.sort((a, b) => a.specialPrice - b.specialPrice);
+    addProducts(sortedList);
+  } else if (e.target.classList.contains("high-to-low")) {
+    sortItems();
+    e.target.id = "selected";
+    sortedList = products.sort((a, b) => b.specialPrice - a.specialPrice);
+    addProducts(sortedList);
+  }
 });
 
-let filterList = document.querySelector(".filters-list")
-    filterList.addEventListener("click", (e)=>{
-        if(e.target.classList.contains("discountsHeading") || e.target.classList.contains("typeHeading")|| e.target.classList.contains("offerHeading") || e.target.classList.contains("brandHeading")|| e.target.classList.contains("cRatingHeading")){
-            e.target.parentElement.classList.toggle("toggleHeight");
-            console.log(e.target);
-        }
-    })
-  
+let filterList = document.querySelector(".filters-list");
+filterList.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("discountsHeading") ||
+    e.target.classList.contains("typeHeading") ||
+    e.target.classList.contains("offerHeading") ||
+    e.target.classList.contains("brandHeading") ||
+    e.target.classList.contains("cRatingHeading")
+  ) {
+    e.target.parentElement.classList.toggle("toggleHeight");
+    e.target.lastElementChild.classList.toggle("toggleArrow");
+  }
+});
+
 
 window.onload = addProducts();

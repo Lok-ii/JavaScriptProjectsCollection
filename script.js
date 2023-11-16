54418f87694372fb20199c57b9cae08563fb1e62
Kaveri Chougule <chougulekaveri123@gmail.com>
2023-11-16 10:00:30 +0530

    Registration and Login forms

--- script.js
+++ script.js
@@ -1,15 +1,72 @@
 let obj={
     project1:{
-        name:"Chrome Extension",
-        image:"https://images.unsplash.com/photo-1480694313141-fce5e697ee25?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        name:"Team",
+        image:"./team.jpg"
     },
     project2:{
-        name:"Resume Template",
-        image:"https://plus.unsplash.com/premium_photo-1661288470388-c5006797bdff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        name:"Chrome Extension",
+        image:"./chromeExtension.jpg"
     },
     project3:{
+        name:"Resume Template",
+        image:"./resumeTemplate.jpg"
+    },
+    project4:{
         name:"Invoice Generator",
-        image:"https://plus.unsplash.com/premium_photo-1679784204535-e623926075cb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        image:"./invoiceGenerator.jpg"
+    },
+    project5:{
+        name:"Payment Integration",
+        image:"./paymentIntegration.jpg"
+    },
+    project6:{
+        name:"Trello Clone",
+        image:"./trelloClone.jpg"
+    },
+    project7:{
+        name:"Typing Speed Test Gamer",
+        image:"./typingSpeedTest.jpg"
+    },
+    project8:{
+        name:"Mine Game",
+        image:"./mineGame.jpg"
+    },
+    project9:{
+        name:"Code Editor",
+        image:"./codeEditor.jpg"
+    },
+    project10:{
+        name:"Flipkart Page sort and filter",
+        image:"./flipkart.jpg"
     }
 }
-console.log(obj);
+
+let signInBtn=document.querySelector(".signInBtn")
+let loginSection=document.querySelector(".loginSection")
+let container=document.querySelector(".container")
+
+signInBtn.addEventListener("click",()=>{
+    container.style.display="none"
+    loginSection.style.display="flex"
+
+})
+let cancel=document.querySelector(".cancel")
+cancel.addEventListener("click",()=>{
+    container.style.display="flex"
+    loginSection.style.display="flex"
+})
+
+let regForm=document.querySelector(".regForm")
+let loginForm=document.querySelector(".loginForm")
+let registration=document.querySelector(".registration")
+let login=document.querySelector(".login")
+registration.addEventListener("click",()=>{
+    console.log("hii");
+    // registration.style.display="flex"
+    // login.style.display="none"
+})
+login.addEventListener("click",()=>{
+    // registration.style.display="none"
+    // login.style.display="flex"
+    console.log("Hello");
+})

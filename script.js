926a604ece2422e91553fc87dd610b4c6b65a466
Kaveri Chougule <chougulekaveri123@gmail.com>
2023-11-17 17:59:36 +0530

    Updated

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
 let obj={
     project1:{
-        name:"Team",
-        image:"../Assets-FrontPage/team.jpg"
-    },
-    project2:{
         name:"Chrome Extension",
         image:"../Assets-FrontPage/chromeExtension.jpg"
     },
-    project3:{
+    project2:{
         name:"Resume Template",
         image:"../Assets-FrontPage/resumeTemplate.jpg"
     },
-    project4:{
+    project3:{
         name:"Invoice Generator",
         image:"../Assets-FrontPage/invoiceGenerator.jpg"
     },
-    project5:{
+    project4:{
         name:"Payment Integration",
         image:"../Assets-FrontPage/paymentIntegration.jpg"
     },
-    project6:{
+    project5:{
         name:"Trello Clone",
         image:"../Assets-FrontPage/trelloClone.jpg"
     },
-    project7:{
+    project6:{
         name:"Typing Speed Test Gamer",
         image:"../Assets-FrontPage/typingSpeedTest.jpg"
     },
-    project8:{
+    project7:{
         name:"Mine Game",
         image:"../Assets-FrontPage/mineGame.jpg"
     },
-    project9:{
+    project8:{
         name:"Code Editor",
         image:"../Assets-FrontPage/codeEditor.jpg"
     },
-    project10:{
+    project9:{
         name:"Flipkart Page sort and filter",
         image:"../Assets-FrontPage/flipkart.jpg"
     }
 }
+const projectObject = Object.getOwnPropertyNames(obj);
+console.log(projectObject.length);
+
 
 let signInBtn=document.querySelector(".signInBtn")
 let loginSection=document.querySelector(".loginSection")
@@ -47,11 +46,15 @@ let container=document.querySelector(".container")
 let registration=document.querySelector(".registration")
 let login=document.querySelector(".login")
 
+// Adding Data dynamically
+window.addEventListener("scroll",()=>{
+    
+})
 
+// SignIn Feature
 signInBtn.addEventListener("click",()=>{
     container.style.display="none"
     loginSection.style.display="flex"
-
 })
 let cancel=document.querySelector(".cancel")
 cancel.addEventListener("click",()=>{
@@ -224,6 +227,5 @@ logOut.addEventListener("click",()=>{
     signInBtnHome.addEventListener("click",()=>{
         container.style.display="none"
         loginSection.style.display="flex"
-        
     })
 })
